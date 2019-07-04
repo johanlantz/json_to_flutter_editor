ab84b5a5fd286895c3d0b47cf0efed5153960e72
Johan Lantz <yc00025@johan-macbook.local>
2019-07-04 18:53:32 +0200

    feat(editor): Basic json text editor with validation but no save

--- lib/components/raw_editor.dart
+++ lib/components/raw_editor.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:json_to_flutter/content/content_handler_registry.dart';
 
@@ -8,7 +10,22 @@ class RawEditor extends StatefulWidget {
 
 class _RawEditorState extends State<RawEditor> {
   Map<String, Map<String, dynamic>> content;
+  List<String> _contentWip = [];
+
   var _controller = TextEditingController();
+
+  _updateContent(String updatedJson) {
+    JsonDecoder decoder = JsonDecoder();
+    var object;
+    try {
+      object = decoder.convert(updatedJson);
+      print('Updated data is valid json.');
+      _contentWip = [];
+    } on FormatException catch (e) {
+      print('Invalid json string at offset ${e.offset}');
+      _controller.selection = TextSelection(baseOffset: e.offset, extentOffset: e.offset-10);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,20 +41,39 @@ class _RawEditorState extends State<RawEditor> {
             return Text('Loading');
           case ConnectionState.active:
             content = snapshot.data;
-            _controller.text = content[content.keys.first].toString();
-
+            JsonEncoder encoder = JsonEncoder.withIndent('  ');
+            if (_contentWip.length > 0) {
+              _controller.text = _contentWip.last;
+            } else {
+              _controller.text = encoder.convert(content[content.keys.first]);
+            }
             return Column(
               children: <Widget>[
                 Row(children: <Widget>[
-                 IconButton(
+                  IconButton(
                     icon: Icon(Icons.check),
                     color: Colors.green,
-                    onPressed: () {},
+                    onPressed: () {
+                      _updateContent(_controller.text);
+                    },
                   ),
-                   IconButton(
+                  IconButton(
+                    icon: Icon(Icons.undo),
+                    color: Colors.red,
+                    onPressed: _contentWip.length == 0 ? null : () {
+                      setState(() {
+                        _contentWip.removeLast();
+                      });
+                    },
+                  ),
+                  IconButton(
                     icon: Icon(Icons.cancel),
                     color: Colors.red,
-                    onPressed: () {},
+                    onPressed: _contentWip.length == 0 ? null : () {
+                      setState(() {
+                        _contentWip = [];
+                      });
+                    },
                   )
                 ]),
                 Expanded(
@@ -45,8 +81,10 @@ class _RawEditorState extends State<RawEditor> {
                         controller: _controller,
                         keyboardType: TextInputType.multiline,
                         maxLines: null,
-                        onSubmitted: (data) {
-                          print('new json is $data');
+                        onChanged: (data) {
+                          setState(() {
+                            _contentWip.add(data);
+                          }); 
                         })),
               ],
             );
